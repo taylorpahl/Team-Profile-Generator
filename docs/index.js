fc419abfffc7dbb66e779fe115f1be3eed3461b0
Taylor P <taylorpahl97@gmail.com>
2023-01-10 16:57:31 -0600

    Added base classes

---
+++ docs/index.js
@@ -0,0 +1,3 @@
+const Employee = require('Employee');
+
+const employee = new Employee();
---
+++ docs/lib/employee.js
@@ -0,0 +1,10 @@
+const inquirer = require('inquirer');
+const Engineer = require('./Engineer');
+const Intern = require('./Intern');
+const Manager = require('./Manager');
+
+class Employee {
+
+}
+
+module.exports = Employee;
